9d191a61cad178585d96bcc5ce1f0394db0d7570
Shrivardhan Gopishetty <gopishettyshrivardhan@gmail.com>
2023-04-27 06:39:29 +0530

    dsa-DAY 8

---
+++ js/dsa-DAY-8.js
@@ -0,0 +1,49 @@
+/**
+ * @description{find first pair from num arr whose sum is equals to x }
+ */
+
+// let x = 5;
+let tArr = (n, x) => {
+    for (let i = 0; i < n.length; i++) {
+        let firstElem = n[i]
+        // console.log(firstElem)
+        for (let j = i + 1; j < n.length; j++) {
+            let secondElem = n[j]
+            // console.log(secondElem)
+            let sum = firstElem + secondElem
+            // console.log(sum)
+            if (sum === x) {
+                return [n[i], n[j]]
+            }
+        }
+    }
+    return []
+}
+console.log(tArr([2, 3, 4, 5, 7, 9], 6))
+
+/**
+ * @description{find first three numbers from number array whose sum is equals to x}
+ */
+
+let x = 6
+const arr = (tp) => {
+    for (let i = 0; i < tp.length; i++) {
+        let a = tp[i]
+        // console.log(a)
+        for (let j = i + 1; j < tp.length; j++) {
+            let b = tp[j]
+            // console.log(b)
+            for (let k = i + 2; k < tp.length; k++) {
+                let c = tp[k]
+                // console.log(c)
+                let sum = a + b + c
+                // console.log(sum)
+                if (sum === x) {
+                    return [tp[i], tp[j], tp[k]]
+                }
+            }
+        }
+    }
+    return []
+}
+console.log(arr([2, 1, 4, 3, 2, 4, 5, 6, 7]))
